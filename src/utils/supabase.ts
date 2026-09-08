811e002949c10c737654d26d57911ec71b032660
TarunSaie <tarun.panchala@wavemaker.com>
2026-09-08 21:53:40 +0530

    chore(config): update supabase credentials to eerxprcgvsgbckbgwboe instance

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -2,9 +2,9 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createClient } from '@supabase/supabase-js';
 
 export const SUPABASE_URL =
-  process.env.EXPO_PUBLIC_SUPABASE_URL || 'https://pmrxrqneaqjqjazfidtf.supabase.co';
+  process.env.EXPO_PUBLIC_SUPABASE_URL || 'https://eerxprcgvsgbckbgwboe.supabase.co';
 export const SUPABASE_ANON_KEY =
-  process.env.EXPO_PUBLIC_SUPABASE_KEY || 'sb_publishable_eLmMv90HOEJNF8K07yaVMg_cqk38Ltr';
+  process.env.EXPO_PUBLIC_SUPABASE_KEY || 'sb_publishable_pf_KSQgSi9XZMLrQjXHa4g_RhUapGrT';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
   auth: {
